58c5def65fb2aee7df298c187c7d5575b7990bcb
neochrome <johan@stenqvist.net>
2011-07-04 12:44:28 +0200

    added simulation support for querying build type details

--- server/server.js
+++ server/server.js
@@ -1,26 +1,46 @@
 var http = require('http').createServer();
 
-var types = {
-	buildType: [
-		{ id: 1, href: '/1', name: 'build 1', projectName: 'project 1', projectId: 1, webUrl: 'chrome://about' },
-		{ id: 2, href: '/2', name: 'build 2', projectName: 'project 2', projectId: 2, webUrl: 'chrome://about' },
-		{ id: 3, href: '/3', name: 'build 3', projectName: 'project 2', projectId: 2, webUrl: 'chrome://about' }
-	]
-};
+var buildTypes = [
+	{ id: 'bt1', href: '/id:bt1', name: 'build 1', description:'description 1', paused: false, webUrl: 'chrome://about', builds: { href: '/id:bt1/builds' }, project: { name: 'project 1', id: 1} },
+	{ id: 'bt2', href: '/id:bt2', name: 'build 2', description:'description 2', paused: false, webUrl: 'chrome://about', builds: { href: '/id:bt2/builds' }, project: { name: 'project 1', id: 2} },
+	{ id: 'bt3', href: '/id:bt3', name: 'build 3', description:'description 3', paused: false, webUrl: 'chrome://about', builds: { href: '/id:bt3/builds' }, project: { name: 'project 2', id: 3} },
+	{ id: 'bt4', href: '/id:bt4', name: 'build 4', description:'description 4', paused: true, webUrl: 'chrome://about', builds: { href: '/id:bt4/builds' }, project: { name: 'project 3', id: 4} }
+];
 
-var builds = {
-	'1': { build: [{ id: 12345, number: '1234', buildTypeId: 1, status: 'SUCCESS' }]},
-	'2': { build: [{ id: 23456, number: '5678', buildTypeId: 2, status: 'FAILURE' }]},
-	'3': { }
-};
+var buildTypesLookup = {};
+var buildTypesIndex = {	buildType: [] };
+var builds = {};
+for(var i = 0; i < buildTypes.length; i++){
+	buildTypesLookup[buildTypes[i].id] = buildTypes[i];
+
+	buildTypesIndex.buildType.push({
+		id: buildTypes[i].id,
+		name: buildTypes[i].name,
+		href: buildTypes[i].href,
+		projectName: buildTypes[i].project.name,
+		projectId: buildTypes[i].project.id,
+		webUrl: buildTypes[i].webUrl
+	});
+
+	if(buildTypes[i].id === 'bt3') { continue; } // unknown / bad build status
+	builds[buildTypes[i].id] = {
+		build: [{ 
+		id: i + 1, 
+		number: i + 1000, 
+		buildTypeId: buildTypes[i].id, 
+		status: i % 2 === 0 ? 'SUCCESS' : 'FAILURE'}]
+	};
+}
 
 http.on('request', function(req, res){
 	res.writeHead(200, {'Content-Type': 'application/json'});
 
-	if(req.url.match(/^\/(\d)/g)){
+	if(req.url.match(/^\/id:(\w+)$/g)){
+		res.end(JSON.stringify(buildTypesLookup[RegExp.$1]));
+	} else if(req.url.match(/^\/id:(\w+)\/builds/g)){
 		res.end(JSON.stringify(builds[RegExp.$1]));
 	} else {
-		res.end(JSON.stringify(types));
+		res.end(JSON.stringify(buildTypesIndex));
 	}
 });
 
